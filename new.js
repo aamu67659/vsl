0341db282d8ff5afe8eb39d5afbeb57286feaaa0
seun <aamu67659@gmail.com>
2025-07-14 09:29:09 -0600

    added javascript

---
+++ new.js
@@ -0,0 +1,20 @@
+alert("hello world")
+var isActive =false;
+console.log(isActive)
+var myname = "seun"
+console.log(myname)
+var userage =20;
+console.log(20)
+console.log (myname,userage)
+var classroom=["ball","close","welcome",23,14,90,0.4,0.10,]
+console.log(classroom[0])
+
+var Estate={
+    location:"ikeja", 
+    price:12000, 
+    address:"No20 awolowo road ikeja",
+    isAvailable:true,
+    isConnected:false,
+}
+console.log(Estate)
+
