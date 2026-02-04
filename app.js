1b6ad483b1bb2505acc001781ddc1819a44e13a2
seun <aamu67659@gmail.com>
2026-02-04 01:35:00 -0600

    started js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.console.log(helloworld);
+const cointainer=document.querySelectorAll(".cointainer");
+cointainer.forEach((el)=>{
+    el.lastElementChild.style.display="none";
+});
+
+cointainer.forEach((el)=>{el.firstElementChild.addEventListener("click",()=>{
+    el.lastElementChild.style.display=
+     el.lastElementChild.style.display===  "none" ? "block" :"none";
+    
+}) ;
+});
+
